90c5452d222f909c0b93639fb65d24a0532ddc4c
fsmosca <fsmosca@users.noreply.github.com>
2022-09-20 17:53:56 +0800

    Update upload.py

--- library/upload.py
+++ library/upload.py
@@ -2,8 +2,8 @@ import streamlit as st
 from io import StringIO
 
 
-def upload_pgn():
-    uploaded_file = st.file_uploader("Choose a file")
+def upload_file():
+    uploaded_file = st.file_uploader("Choose a file", type=['json'])
     if uploaded_file is not None:
         return StringIO(uploaded_file.getvalue().decode("utf-8"))
     return None
